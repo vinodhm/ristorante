c386e355bcddb989aaf6169f08e6ef89fe22c64f
vinodhm <vinodh.manda@gmail.com>
2019-12-26 01:58:41 -0500

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,11 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 
 class Main extends Component {
@@ -12,7 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -23,7 +29,11 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-          <Home />
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
 
@@ -33,7 +43,8 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Redirect to="/home" />
+              <Route exact path='/contactus' component={Contact} />} />
+              <Redirect to="/home" />              
           </Switch>
         {/* <Menu dishes={this.state.dishes} 
          onClick={(dishId) => this.onDishSelect(dishId)}/>
